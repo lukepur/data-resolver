0e5e767ce11da7aa8742b999bd06ee62c5ad38f7
Luke Purton <luke.purton@gmail.com>
2017-09-28 11:49:40 +1000

    Add some more complicated sample resolvables

--- docs/src/sample/sample-resolvables.js
+++ docs/src/sample/sample-resolvables.js
@@ -32,5 +32,173 @@ export default () => [
         }
       ]
     }
+  },
+
+  {
+    id: 'actors_in_multiple_movies',
+    resolvable: {
+      resolvableType: 'fn',
+      value: 'keys',
+      args: [
+        {
+          resolvableType: 'fn',
+          value: 'pickBy',
+          args: [
+            {
+              resolvableType: 'fn',
+              value: 'groupBy',
+              args: [
+                {
+                  resolvableType: 'fn',
+                  value: 'flatten',
+                  args: [{ resolvableType: 'lookup', value: '$.movies.*.actors' }]
+                }
+              ]
+            },
+            {
+              resolvableType: 'fn',
+              value: 'overArgs',
+              args: [
+                {
+                  resolvableType: 'fn',
+                  value: 'partial',
+                  args: [
+                    { resolvableType: 'fnRefLookup', value: 'gt' },
+                    { resolvableType: 'fnRefLookup', value: '_' },
+                    { resolvableType: 'literal', value: 1 }
+                  ]
+                },
+                {
+                  resolvableType: 'fn',
+                  value: 'partial',
+                  args: [
+                    { resolvableType: 'fnRefLookup', value: 'get' },
+                    { resolvableType: 'fnRefLookup', value: '_' },
+                    { resolvableType: 'literal', value: 'length' }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  },
+
+  {
+    id: 'movies_with_shared_actors',
+    resolvable: {
+      resolvableType: 'fn',
+      value: 'filter',
+      args: [
+        { resolvableType: 'lookup', value: '$.movies.*' },
+        {
+          resolvableType: 'fn',
+          value: 'overArgs',
+          args: [
+            {
+              resolvableType: 'fn',
+              value: 'partial',
+              args: [
+                { resolvableType: 'fnRefLookup', value: 'gt' },
+                { resolvableType: 'fnRefLookup', value: '_' },
+                { resolvableType: 'literal', value: 0 }
+              ]
+            },
+            {
+              resolvableType: 'fn',
+              value: 'overArgs',
+              args: [
+                {
+                  resolvableType: 'fn',
+                  value: 'partial',
+                  args: [
+                    { resolvableType: 'fnRefLookup', value: 'get' },
+                    { resolvableType: 'fnRefLookup', value: '_' },
+                    { resolvableType: 'literal', value: 'length' }
+                  ]
+                },
+                {
+                  resolvableType: 'fn',
+                  value: 'overArgs',
+                  args: [
+                    {
+                      resolvableType: 'fn',
+                      value: 'partial',
+                      args: [
+                        {
+                          resolvableType: 'fnRefLookup',
+                          value: 'intersection'
+                        },
+                        {
+                          resolvableType: 'fnRefLookup',
+                          value: '_'
+                        },
+                        {
+                          resolvableType: 'fn', // This resolvable is identical to actors_in_multiple_movies
+                          value: 'keys',
+                          args: [
+                            {
+                              resolvableType: 'fn',
+                              value: 'pickBy',
+                              args: [
+                                {
+                                  resolvableType: 'fn',
+                                  value: 'groupBy',
+                                  args: [
+                                    {
+                                      resolvableType: 'fn',
+                                      value: 'flatten',
+                                      args: [{ resolvableType: 'lookup', value: '$.movies.*.actors' }]
+                                    }
+                                  ]
+                                },
+                                {
+                                  resolvableType: 'fn',
+                                  value: 'overArgs',
+                                  args: [
+                                    {
+                                      resolvableType: 'fn',
+                                      value: 'partial',
+                                      args: [
+                                        { resolvableType: 'fnRefLookup', value: 'gt' },
+                                        { resolvableType: 'fnRefLookup', value: '_' },
+                                        { resolvableType: 'literal', value: 1 }
+                                      ]
+                                    },
+                                    {
+                                      resolvableType: 'fn',
+                                      value: 'partial',
+                                      args: [
+                                        { resolvableType: 'fnRefLookup', value: 'get' },
+                                        { resolvableType: 'fnRefLookup', value: '_' },
+                                        { resolvableType: 'literal', value: 'length' }
+                                      ]
+                                    }
+                                  ]
+                                }
+                              ]
+                            }
+                          ]
+                        }
+                      ]
+                    },
+                    {
+                      resolvableType: 'fn',
+                      value: 'partial',
+                      args: [
+                        { resolvableType: 'fnRefLookup', value: 'get' },
+                        { resolvableType: 'fnRefLookup', value: '_' },
+                        { resolvableType: 'literal', value: 'actors' }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
   }
 ];
